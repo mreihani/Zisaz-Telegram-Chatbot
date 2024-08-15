77c38b491cb00ed2111f245d3bc37bbd5c8bf07e
Your Nam <you@example.co>
2024-08-15 03:32:51 +0000

    Add return to all methods in TelegramController to increase performance

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -54,238 +54,238 @@ class TelegramController extends Controller
         if($incoming_text === '/start') {
             $isCommand = true;
             $startSection = new StartSection($telegram);
-            $startSection->displayItem();
+            return $startSection->displayItem();
 
             // محاسبات زیربنا، هزینه و  مشارکت در ساخت     
         } elseif($incoming_text === '/getconstructioncalculation' || $incoming_text == '👷🏽 محاسبات زیربنا، هزینه و مشارکت در ساخت') {
             $isCommand = true;
             $constructionCalculationSection = new ConstructionCalculation($telegram);
-            $constructionCalculationSection->displayItem();
+            return $constructionCalculationSection->displayItem();
         } elseif($incoming_text === '/constructionsendpamameteratext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $isCommand = true;
             $constructionBotResponse = new ConstructionBotResponse($telegram);
-            $constructionBotResponse->processParameterSubmission();
+            return $constructionBotResponse->processParameterSubmission();
         } elseif($incoming_text === '/constructioncalcexpensedownloadresults') {
             // دانلود پی دی اف محاسبات  کل  زیر بنا و هزینه ساخت
             $isCommand = true;
             $constructionBotResponse = new ConstructionBotResponse($telegram);
-            $constructionBotResponse->downloadConstCalcExpenseResults();
+            return $constructionBotResponse->downloadConstCalcExpenseResults();
         } elseif($incoming_text === '/constructioncalccollaborativedownloadresults') {
             // دانلود پی دی اف محاسبات  مشارکت در ساخت منصفانه
             $isCommand = true;
             $constructionBotResponse = new ConstructionBotResponse($telegram);
-            $constructionBotResponse->downloadConstCalcCollaborativeResults();
+            return $constructionBotResponse->downloadConstCalcCollaborativeResults();
         } elseif($incoming_text === '/constructionresetresults') {
             // پروژه جدید
             $isCommand = true;
             $constructionBotResponse = new ConstructionBotResponse($telegram);
-            $constructionBotResponse->resetResults();
+            return $constructionBotResponse->resetResults();
             
             // محاسبات سقف تیرچه و بلوک
         } elseif($incoming_text === '/getbeamandblockroof' || $incoming_text == '🏗️ سقف تیرچه و بلوک') {
             $isCommand = true;
             $beamAndBlockRoofCalculation = new BeamAndBlockRoofCalculation($telegram);
-            $beamAndBlockRoofCalculation->displayItem();
+            return $beamAndBlockRoofCalculation->displayItem();
         } elseif($incoming_text === '/beamandblockroofsendpamameteratext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $isCommand = true;
             $beamAndBlockRoofBotResponse = new BeamAndBlockRoofBotResponse($telegram);
-            $beamAndBlockRoofBotResponse->processParameterSubmission();
+            return $beamAndBlockRoofBotResponse->processParameterSubmission();
         } elseif($incoming_text === '/beamandblockroofdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $beamAndBlockRoofBotResponse = new BeamAndBlockRoofBotResponse($telegram);
-            $beamAndBlockRoofBotResponse->downloadResults();
+            return $beamAndBlockRoofBotResponse->downloadResults();
         } elseif($incoming_text === '/beamandblockroofresetresults') {
             // پروژه جدید
             $isCommand = true;
             $beamAndBlockRoofBotResponse = new BeamAndBlockRoofBotResponse($telegram);
-            $beamAndBlockRoofBotResponse->resetResults();
+            return $beamAndBlockRoofBotResponse->resetResults();
             
             // محاسبات دیوار چینی بلوکی و آجری 
         } elseif($incoming_text === '/getbrickwallmasonry' || $incoming_text == '🧱 دیوار چینی') {
             $isCommand = true;
             $brickWallMasonryCalculation = new BrickWallMasonryCalculation($telegram);
-            $brickWallMasonryCalculation->displayItem();
+            return $brickWallMasonryCalculation->displayItem();
             
             // دیوار پارتیشن بلوکی آپارتمان
         } elseif($incoming_text === '/brickwallmasonryapartmentblock') {
             $isCommand = true;
             $brickWallMasonryApartmentBlock = new BrickWallMasonryApartmentBlockCalculation($telegram);
-            $brickWallMasonryApartmentBlock->displayItem();
+            return $brickWallMasonryApartmentBlock->displayItem();
         } elseif($incoming_text === '/brickwallmasonryapartmentblocksendpamameteratext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $brickWallMasonryApartmentBlock = new BrickWallMasonryApartmentBlockBotResponse($telegram);
-            $brickWallMasonryApartmentBlock->processParameterSubmission();
+            return $brickWallMasonryApartmentBlock->processParameterSubmission();
         } elseif($incoming_text === '/brickwallmasonryapartmentblockdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $brickWallMasonryApartmentBlock = new BrickWallMasonryApartmentBlockBotResponse($telegram);
-            $brickWallMasonryApartmentBlock->downloadResults();
+            return $brickWallMasonryApartmentBlock->downloadResults();
         } elseif($incoming_text === '/brickwallmasonryapartmentblockresetresults') {
             // پروژه جدید
             $isCommand = true;
             $brickWallMasonryApartmentBlock = new BrickWallMasonryApartmentBlockBotResponse($telegram);
-            $brickWallMasonryApartmentBlock->resetResults();
+            return $brickWallMasonryApartmentBlock->resetResults();
 
             // دیوار بلوکی حصار باغ یا حیاط 
         } elseif($incoming_text === '/brickwallmasonrygarden') {
             $isCommand = true;
             $brickWallMasonryGarden = new BrickWallMasonryGardenCalculation($telegram);
-            $brickWallMasonryGarden->displayItem();
+            return $brickWallMasonryGarden->displayItem();
         } elseif($incoming_text === '/brickwallmasonrygardensendpamameterltext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $brickWallMasonryGarden = new BrickWallMasonryGardenBotResponse($telegram);
-            $brickWallMasonryGarden->processParameterSubmission();
+            return $brickWallMasonryGarden->processParameterSubmission();
         } elseif($incoming_text === '/brickwallmasonrygardendownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $brickWallMasonryGarden = new BrickWallMasonryGardenBotResponse($telegram);
-            $brickWallMasonryGarden->downloadResults();
+            return $brickWallMasonryGarden->downloadResults();
         } elseif($incoming_text === '/brickwallmasonrygardenresetresults') {
             // پروژه جدید
             $isCommand = true;
             $brickWallMasonryGarden = new BrickWallMasonryGardenBotResponse($telegram);
-            $brickWallMasonryGarden->resetResults();
+            return $brickWallMasonryGarden->resetResults();
 
             // آجر فشاری یا سه گل 
         } elseif($incoming_text === '/brickwallmasonrypressedbrick') {
             $isCommand = true;
             $brickWallMasonryPressedBrick = new BrickWallMasonryPressedBrickCalculation($telegram);
-            $brickWallMasonryPressedBrick->displayItem();
+            return $brickWallMasonryPressedBrick->displayItem();
         } elseif($incoming_text === '/brickwallmasonrypressedbricksendpamameteratext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $brickWallMasonryPressedBrick = new BrickWallMasonryPressedBrickBotResponse($telegram);
-            $brickWallMasonryPressedBrick->processParameterSubmission();
+            return $brickWallMasonryPressedBrick->processParameterSubmission();
         } elseif($incoming_text === '/brickwallmasonrypressedbrickdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $brickWallMasonryPressedBrick = new BrickWallMasonryPressedBrickBotResponse($telegram);
-            $brickWallMasonryPressedBrick->downloadResults();
+            return $brickWallMasonryPressedBrick->downloadResults();
         } elseif($incoming_text === '/brickwallmasonrypressedbrickresetresults') {
             // پروژه جدید
             $isCommand = true;
             $brickWallMasonryPressedBrick = new BrickWallMasonryPressedBrickBotResponse($telegram);
-            $brickWallMasonryPressedBrick->resetResults();
+            return $brickWallMasonryPressedBrick->resetResults();
             
             // دیوار با آجر پارتیشن
         } elseif($incoming_text === '/brickwallmasonrypartition') {
             $isCommand = true;
             $brickWallMasonryPartition = new BrickWallMasonryPartitionCalculation($telegram);
-            $brickWallMasonryPartition->displayItem();
+            return $brickWallMasonryPartition->displayItem();
         } elseif($incoming_text === '/brickwallmasonrypartitionsendpamameteratext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $brickWallMasonryPartition = new BrickWallMasonryPartitionBotResponse($telegram);
-            $brickWallMasonryPartition->processParameterSubmission();
+            return $brickWallMasonryPartition->processParameterSubmission();
         } elseif($incoming_text === '/brickwallmasonrypartitiondownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $brickWallMasonryPartition = new BrickWallMasonryPartitionBotResponse($telegram);
-            $brickWallMasonryPartition->downloadResults();
+            return $brickWallMasonryPartition->downloadResults();
         } elseif($incoming_text === '/brickwallmasonrypartitionresetresults') {
             // پروژه جدید
             $isCommand = true;
             $brickWallMasonryPartition = new BrickWallMasonryPartitionBotResponse($telegram);
-            $brickWallMasonryPartition->resetResults();
+            return $brickWallMasonryPartition->resetResults();
             
             // محاسبات رمپ و درز انقطاع
         } elseif($incoming_text === '/getrampandexpansionjoint' || $incoming_text == '📐 محاسبات رمپ و درز انقطاع') {
             $isCommand = true;
             $rampAndExpansionJointCalculation = new RampAndExpansionJointCalculation($telegram);
-            $rampAndExpansionJointCalculation->displayItem();
+            return $rampAndExpansionJointCalculation->displayItem();
 
             // شیب رمپ
         } elseif($incoming_text === '/rampsteep') {
             $isCommand = true;
             $rampSteep = new RampSteepCalculation($telegram);
-            $rampSteep->displayItem();
+            return $rampSteep->displayItem();
         } elseif($incoming_text === '/rampsteepsendpamameterhtext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $rampSteep = new RampSteepBotResponse($telegram);
-            $rampSteep->processParameterSubmission();
+            return $rampSteep->processParameterSubmission();
         } elseif($incoming_text === '/rampsteepdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $rampSteep = new RampSteepBotResponse($telegram);
-            $rampSteep->downloadResults();
+            return $rampSteep->downloadResults();
         } elseif($incoming_text === '/rampsteepresetresults') {
             // پروژه جدید
             $isCommand = true;
             $rampSteep = new RampSteepBotResponse($telegram);
-            $rampSteep->resetResults();
+            return $rampSteep->resetResults();
 
             // طول رمپ
         } elseif($incoming_text === '/ramplength') {
             $isCommand = true;
             $rampLength = new RampLengthCalculation($telegram);
-            $rampLength->displayItem();
+            return $rampLength->displayItem();
         } elseif($incoming_text === '/ramplengthsendpamameterhtext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $rampLength = new RampLengthBotResponse($telegram);
-            $rampLength->processParameterSubmission();
+            return $rampLength->processParameterSubmission();
         } elseif($incoming_text === '/ramplengthdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $rampLength = new RampLengthBotResponse($telegram);
-            $rampLength->downloadResults();
+            return $rampLength->downloadResults();
         } elseif($incoming_text === '/ramplengthresetresults') {
             // پروژه جدید
             $isCommand = true;
             $rampLength = new RampLengthBotResponse($telegram);
-            $rampLength->resetResults();
+            return $rampLength->resetResults();
 
             // محاسبه درز انقطاع ( ژوئن)
         } elseif($incoming_text === '/expansionjoint') {
             $isCommand = true;
             $expansionJoint = new ExpansionJointCalculation($telegram);
-            $expansionJoint->displayItem();
+            return $expansionJoint->displayItem();
         } elseif($incoming_text === '/expansionjointsendpamameterhtext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $expansionJoint = new ExpansionJointBotResponse($telegram);
-            $expansionJoint->processParameterSubmission();
+            return $expansionJoint->processParameterSubmission();
         } elseif($incoming_text === '/expansionjointdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $expansionJoint = new ExpansionJointBotResponse($telegram);
-            $expansionJoint->downloadResults();
+            return $expansionJoint->downloadResults();
         } elseif($incoming_text === '/expansionjointresetresults') {
             // پروژه جدید
             $isCommand = true;
             $expansionJoint = new ExpansionJointBotResponse($telegram);
-            $expansionJoint->resetResults();
+            return $expansionJoint->resetResults();
 
             // محاسبات وزن میلگرد و خاموت
         } elseif($incoming_text === '/getrebarandstirrup' || $incoming_text == '➰ وزن میلگرد و خاموت') {
             $isCommand = true;
             $rebarAndStirrupCalculation = new RebarAndStirrupCalculation($telegram);
-            $rebarAndStirrupCalculation->displayItem();
+            return $rebarAndStirrupCalculation->displayItem();
 
             // محاسبه وزن یک شاخه میلگرد
         } elseif($incoming_text === '/rebarweight') {
             $isCommand = true;
             $rebarWeight = new RebarWeightCalculation($telegram);
-            $rebarWeight->displayItem();
+            return $rebarWeight->displayItem();
         } elseif($incoming_text === '/rebarweightsendpamameterdtext') {
             // دریافت اطلاعات و پارامتر های محاسباتی
             $rebarWeight = new RebarWeightBotResponse($telegram);
-            $rebarWeight->processParameterSubmission();
+            return $rebarWeight->processParameterSubmission();
         } elseif($incoming_text === '/rebarweightdownloadresults') {
             // دانلود پی دی اف
             $isCommand = true;
             $rebarWeight = new RebarWeightBotResponse($telegram);
-            $rebarWeight->downloadResults();
+            return $rebarWeight->downloadResults();
         } elseif($incoming_text === '/rebarweightresetresults') {
             // پروژه جدید
             $isCommand = true;
             $rebarWeight = new RebarWeightBotResponse($telegram);
-            $rebarWeight->resetResults();
+            return $rebarWeight->resetResults();
 
             // دریافت کلیه ورودی های تایپ شده کاربر
         } elseif(!$isCommand) {
             try {
                 $userPrompts = new UserPrompts($telegram);
-                $userPrompts->checkUserPrompt();
+                return $userPrompts->checkUserPrompt();
             } catch(Exception $e) {
                 \Log::info('An error occurred: ' . $e->getMessage());
             }
